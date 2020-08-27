Liwukong/Julia

commit 35c1f87176fecab5a8e8077fb3a62275363a5aa5

fix #36230, more efficient lowering of `if` with a chain of `&&` (#36355)

src/julia-syntax.scm
@@ -1895,6 +1895,13 @@
                 (else
                  (error (string "invalid " syntax-str " \"" (deparse el) "\""))))))))
 
+(define (expand-if e)
+  (if (and (pair? (cadr e)) (eq? (car (cadr e)) '&&))
+      (let ((clauses (cdr (flatten-ex '&& (cadr e)))))
+        `(if (&& ,@(map expand-forms clauses))
+             ,@(map expand-forms (cddr e))))
+      (cons (car e) (map expand-forms (cdr e)))))
+
 ;; move an assignment into the last statement of a block to keep more statements at top level
 (define (sink-assignment lhs rhs)
   (if (and (pair? rhs) (eq? (car rhs) 'block))
@@ -2229,6 +2236,9 @@
          `(call (core typeassert)
                 ,(expand-forms (cadr e)) ,(expand-forms (caddr e)))
          (map expand-forms e)))
+
+   'if expand-if
+   'elseif expand-if
 
    'while
    (lambda (e)
@@ -3709,7 +3719,8 @@ f(x) = yt(x)
         (handler-level 0)     ;; exception handler nesting depth
         (catch-token-stack '())) ;; tokens identifying handler enter for current catch blocks
     (define (emit c)
-      (set! code (cons c code)))
+      (set! code (cons c code))
+      c)
     (define (make-label)
       (begin0 label-counter
               (set! label-counter (+ 1 label-counter))))
@@ -3957,15 +3968,21 @@ f(x) = yt(x)
                  (compile (cadr e) break-labels value tail)
                  #f))
             ((if elseif)
-             (let ((test `(gotoifnot ,(compile-cond (cadr e) break-labels) _))
+             (let ((tests (map (lambda (clause)
+                                 (emit `(gotoifnot ,(compile-cond clause break-labels) _)))
+                               (if (and (pair? (cadr e)) (eq? (car (cadr e)) '&&))
+                                   (cdadr e)
+                                   (list (cadr e)))))
                    (end-jump `(goto _))
                    (val (if (and value (not tail)) (new-mutable-var) #f)))
-               (emit test)
                (let ((v1 (compile (caddr e) break-labels value tail)))
                  (if val (emit-assignment val v1))
                  (if (and (not tail) (or (length> e 3) val))
                      (emit end-jump))
-                 (set-car! (cddr test) (make&mark-label))
+                 (let ((elselabel (make&mark-label)))
+                   (for-each (lambda (test)
+                               (set-car! (cddr test) elselabel))
+                             tests))
                  (let ((v2 (if (length> e 3)
                                (compile (cadddr e) break-labels value tail)
                                '(null))))

base/expr.jl
@@ -347,9 +347,9 @@ function findmeta_block(exargs, argsmatch=args->true)
     for i = 1:length(exargs)
         a = exargs[i]
         if isa(a, Expr)
-            if (a::Expr).head === :meta && argsmatch((a::Expr).args)
+            if a.head === :meta && argsmatch(a.args)
                 return i, exargs
-            elseif (a::Expr).head === :block
+            elseif a.head === :block
                 idx, exa = findmeta_block(a.args, argsmatch)
                 if idx != 0
                     return idx, exa

base/meta.jl
@@ -62,8 +62,9 @@ true
 ```
 """
 isexpr(@nospecialize(ex), head::Symbol) = isa(ex, Expr) && ex.head === head
-isexpr(@nospecialize(ex), heads::Union{Set,Vector,Tuple}) = isa(ex, Expr) && in(ex.head, heads)
-isexpr(@nospecialize(ex), heads, n::Int) = isexpr(ex, heads) && length((ex::Expr).args) == n
+isexpr(@nospecialize(ex), heads) = isa(ex, Expr) && in(ex.head, heads)
+isexpr(@nospecialize(ex), head::Symbol, n::Int) = isa(ex, Expr) && ex.head === head && length(ex.args) == n
+isexpr(@nospecialize(ex), heads, n::Int) = isa(ex, Expr) && in(ex.head, heads) && length(ex.args) == n
 
 """
     Meta.show_sexpr([io::IO,], ex)

test/compiler/inference.jl
@@ -2650,3 +2650,13 @@ end
 f(n) = depth(n, 1)
 end
 @test Base.return_types(TestConstPropRecursion.f, (TestConstPropRecursion.Node,)) == Any[Int]
+
+# issue #36230, keeping implications of all conditions in a && chain
+function symcmp36230(vec)
+    a, b = vec[1], vec[2]
+    if isa(a, Symbol) && isa(b, Symbol)
+        return a == b
+    end
+    return false
+end
+@test Base.return_types(symcmp36230, (Vector{Any},)) == Any[Bool]

commit 88cf80a4de599259a812689a88539897809efcf2

fix #37228, lowering regression in `if a && ...`
caused by #36355

src/julia-syntax.scm
@@ -3992,11 +3992,16 @@ f(x) = yt(x)
                  (compile (cadr e) break-labels value tail)
                  #f))
             ((if elseif)
-             (let ((tests (map (lambda (clause)
-                                 (emit `(gotoifnot ,(compile-cond clause break-labels) _)))
-                               (if (and (pair? (cadr e)) (eq? (car (cadr e)) '&&))
-                                   (cdadr e)
-                                   (list (cadr e)))))
+             (let ((tests (let* ((cond (cadr e))
+                                 (cond (if (and (pair? cond) (eq? (car cond) 'block))
+                                           (begin (compile (butlast cond) break-labels #f #f)
+                                                  (last cond))
+                                           cond)))
+                            (map (lambda (clause)
+                                   (emit `(gotoifnot ,(compile-cond clause break-labels) _)))
+                                 (if (and (pair? cond) (eq? (car cond) '&&))
+                                     (cdr cond)
+                                     (list cond)))))
                    (end-jump `(goto _))
                    (val (if (and value (not tail)) (new-mutable-var) #f)))
                (let ((v1 (compile (caddr e) break-labels value tail)))

test/syntax.jl
@@ -2325,3 +2325,14 @@ end
 @test_throws ParseError("invalid operator \".<---\"") Meta.parse("1 .<--- 2")
 @test_throws ParseError("invalid operator \"--\"") Meta.parse("a---b")
 @test_throws ParseError("invalid operator \".--\"") Meta.parse("a.---b")
+
+# issue #37228
+function f37228(x)
+    if isodd(x[1]) && all(iseven(y) for y in x[2:end])
+        return 3
+    end
+    return 0
+end
+@test f37228([1,2,4,8]) == 3
+@test f37228([2,2,4,8]) == 0
+@test f37228([3,3,4,8]) == 0